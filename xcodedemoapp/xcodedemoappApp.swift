bc4e052c086b681b0b392034c3c0bc9af00ea1dc
hardik <hnoob2992@gmail.com>
2024-10-27 11:26:46 -0700

    Initial Commit

---
+++ xcodedemoapp/xcodedemoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodedemoappApp.swift
+//  xcodedemoapp
+//
+//  Created by HARDIK SINGH CHOUHANH on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct xcodedemoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xcodedemoappTests/xcodedemoappTests.swift
@@ -0,0 +1,17 @@
+//
+//  xcodedemoappTests.swift
+//  xcodedemoappTests
+//
+//  Created by HARDIK SINGH CHOUHANH on 27/10/24.
+//
+
+import Testing
+@testable import xcodedemoapp
+
+struct xcodedemoappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
